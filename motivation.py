67ba4bc4b7bc6ea1c6592309551bf1751702828e
Neylson Crepalde <neylsoncrepalde@gmail.com>
2017-08-10 01:51:50 -0300

    Create motivation.py

---
+++ motivation.py
@@ -0,0 +1,42 @@
+"""
+Posts de motivação
+Bot Educação Superior
+@author: neylson.crepalde
+"""
+from twython import Twython
+import random
+import time
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+twitter = Twython(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+messages = ["Aprender é a única coisa que a mente nunca se cansa, nunca tem medo e nunca se arrepende!",
+            "Aquele que não luta pelo futuro que quer, deve aceitar o futuro que vier!",
+            "Ter sucesso é falhar repetidamente, mas sem perder o entusiasmo!",
+            "Não importa o quão devagar você vá, desde que você não pare!",
+            "Descobri que quanto mais eu estudo, mais sorte eu pareço ter nas provas!",
+            "É fazendo que se aprende, aquilo que se deve aprender a fazer!",
+            "Nenhum obstáculo é tão grande se sua vontade de vencer for maior!",
+            "Há cinco degraus para se alcançar a sabedoria: Calar, ouvir, lembrar, sair, estudar.",
+            "Comece onde está. Use o que você tem. Faça o que puder!",
+            "O sucesso normalmente vem para quem está ocupado demais para pensar nele!",
+            "Você já observou um homem habilidoso em seu trabalho? Será promovido ao serviço real; não trabalhará para gente obscura."]
+#twitter.update_status(status=message)
+#print("Tweeted: {}".format(message))
+
+pics = ['aYzlNG5L.jpg','chapeu.jpeg','chapeu2.jpg','chapeu3.jpg','ensinosuperior.jpg',
+        'estudante.jpg','livro.jpg','net.jpg']
+
+while True:
+    with open(random.choice(pics), 'rb') as photo:
+        twitter.upload_media(status=random.choice(messages), media=photo)
+    print("Tweet postado!")
+    time.sleep(17280)
